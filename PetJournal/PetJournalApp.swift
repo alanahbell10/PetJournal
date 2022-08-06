13f514d739d0769232771078cce8454b3b1d5a0f
alanahbell10 <abell22@students.towson.edu>
2022-08-06 18:45:20 -0400

    Initial Commit

---
+++ PetJournal/PetJournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetJournalApp.swift
+//  PetJournal
+//
+//  Created by Alanah Bell on 8/6/22.
+//
+
+import SwiftUI
+
+@main
+struct PetJournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
